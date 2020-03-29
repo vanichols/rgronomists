46a605dc781a29844b8e3e33da0ecd49d3a3ee96
Unknown <vnichols@iastate.edu>
2020-03-29 12:51:01 -0500

    fix yield col names (final_yield vs yield)

--- data-raw/ncyc/code_nycy.R
+++ data-raw/ncyc/code_nycy.R
@@ -10,29 +10,27 @@ library(lubridate)
 
 # 2013 --------------------------------------------------------------------
 
-dat13 <- read_excel("data-raw/ncyc/NCYC_corn-yield-winners-2013-2019.xlsx", sheet = "2013")
+dat13 <- read_excel("data-raw/ncyc/NCYC_corn-yield-winners-2013-2019.xlsx", sheet = "2013") %>%
+  clean_names() %>%
+  mutate(recheck = ifelse(recheck == T, "First Check", "Recheck")) %>%
+  rename(yield = final_yield)
 
-#--michigan has an untrue value in date_planted, others in date_harvested
+#--michigan has an untrue value in date_planted, others in date_harvested, fix at end
 dat13 %>%
   clean_names() %>%
   select(contains("date")) %>%
   rowid_to_column() %>%
   filter(is.na(date_harvested))
 
-#--just fill value from the same state
-dat13_dates <-
-  dat13 %>%
-  clean_names() %>%
-  group_by(field_state) %>%
-  fill(date_planted, date_harvested) %>%
-  mutate(recheck = ifelse(recheck == T, "First Check", "Recheck"))
 
 
 # 2014 --------------------------------------------------------------------
 
 dat14 <- read_excel("data-raw/ncyc/NCYC_corn-yield-winners-2013-2019.xlsx", sheet = "2014") %>%
   clean_names() %>%
-  mutate(recheck = ifelse(recheck == T, "First Check", "Recheck"))
+  mutate(recheck = ifelse(recheck == T, "First Check", "Recheck")) %>%
+  rename(yield = final_yield)
+
 
 
 # 2015 --------------------------------------------------------------------
@@ -40,7 +38,9 @@ dat14 <- read_excel("data-raw/ncyc/NCYC_corn-yield-winners-2013-2019.xlsx", shee
 
 dat15 <- read_excel("data-raw/ncyc/NCYC_corn-yield-winners-2013-2019.xlsx", sheet = "2015") %>%
   clean_names() %>%
-  mutate(recheck = ifelse(recheck == T, "First Check", "Recheck"))
+  mutate(recheck = ifelse(recheck == T, "First Check", "Recheck")) %>%
+  rename(yield = final_yield)
+
 
 dat15 %>%
   select(recheck)
@@ -50,7 +50,9 @@ dat15 %>%
 
 # don't worry about warnings
 dat16 <- read_excel("data-raw/ncyc/NCYC_corn-yield-winners-2013-2019.xlsx", sheet = "2016") %>%
-  clean_names()
+  clean_names() %>%
+  rename(yield = final_yield)
+
 
 
 # 2017 --------------------------------------------------------------------
@@ -78,16 +80,20 @@ dat19 <- read_excel("data-raw/ncyc/NCYC_corn-yield-winners-2013-2019.xlsx", shee
 
 
 ncyc_raw <-
-  bind_rows(dat13_dates, dat14, dat15, dat16, dat17, dat18, dat19) %>%
+  bind_rows(dat13, dat14, dat15, dat16, dat17, dat18, dat19) %>%
   mutate(year = year(date_planted)) %>%
+  #--just fill value from the same state
+  group_by(year, field_state) %>%
+  fill(date_planted, date_harvested) %>%
   arrange(year, field_state, state_rank) %>%
   mutate(date_planted = as_date(date_planted),
-         date_harvested = as_date(date_harvested))
-
-
+         date_harvested = as_date(date_harvested)) %>%
+  select(year, everything())
 
 ncyc_simp <-
   ncyc_raw %>%
+  # why is there a 2012 SC?
+  filter(year > 2012) %>%
   select(year,
          national_rank,
          state_rank,
@@ -99,6 +105,7 @@ ncyc_simp <-
          date_harvested,
          planting_rate,
          row_spacing,
+         irrigated,
          nitrogen,
          phosphorus,
          potash,
@@ -109,8 +116,24 @@ ncyc_simp <-
          acres_harvested, total_bushels_harvested, bushels_per_acres,
          acres_harvested_recheck, total_bushels_harvested_recheck, bushels_per_acres_recheck)
 
-ncyc_simp %>% write_csv("data-raw/ncyc/nycy_simp.csv")
+library(ggforce)
+ncyc_simp %>%
+  ggplot(aes(as.factor(year), yield)) +
+  geom_sina(aes(color = irrigated))
+
+ncyc_simp %>%
+  filter(state_rank == "1") %>%
+  ggplot(aes(reorder(field_state, yield, median), yield)) +
+  geom_sina(aes(color = irrigated))
+
+ncyc_simp %>%
+  filter(state_rank == "1") %>%
+  ggplot(aes(reorder(field_state, yield, median), yield)) +
+  geom_jitter(aes(color = irrigated))
+
+
+ncyc_simp %>% write_csv("data-raw/ncyc/ncyc_simp.csv")
 use_data(ncyc_simp, overwrite = T)
 
-ncyc_raw %>% write_csv("data-raw/ncyc/nycy_raw.csv")
+ncyc_raw %>% write_csv("data-raw/ncyc/ncyc_raw.csv")
 use_data(ncyc_raw, overwrite = T)
